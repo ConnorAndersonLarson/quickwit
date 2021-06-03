033bd7bbeec5aa4f9cdd4823674cc3bf11a01a69
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-06-02 21:36:36 -0500

    Add styling to tiles

--- src/Tile/Tile.js
+++ src/Tile/Tile.js
@@ -2,11 +2,16 @@ import './Tile.css';
 
 const Tile = ( { title, author, published } ) => {
 
+  const cleanDate = () => {
+    return published.substr(0,10)
+  }
+
+
   return(
     <section className="tile">
       <h3>{title}</h3>
       <h4>{author}</h4>
-      <h4>{published}</h4>
+      <h4>{cleanDate()}</h4>
     </section>
   )
 }
